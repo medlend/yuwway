4cadce07f33149596bcf38eb140b538fd9918ce8
lendolsi <mohamed.lendolsi@gmail.com>
2016-11-15 15:25:12 +0100

    add captcha

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
+            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
             new Yuwway\UserBundle\YuwwayUserBundle(),
         ];
 
---
+++ src/Yuwway/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 15/11/16
+ * Time: 11:21
+ */
+
+namespace Yuwway\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('captcha', 'Genemu\Bundle\FormBundle\Form\Core\Type\CaptchaType',array('mapped' => false,));
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'yuwway_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
